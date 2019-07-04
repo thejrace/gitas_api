a8e3bb207c83a3953b1ec98800b7fc4a41b81f52
Obarey <ahmetkanbur@gmail.com>
2019-07-04 16:37:06 +0200

    correction: busses to buses

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,7 @@ use App\Http\Controllers\Api\BusController;
 
 Route::middleware(['auth:api'])->group(function(){
 
-    Route::resource('busses', BusController::class );
-
-
+    Route::resource('buses', BusController::class );
 
 });
 
