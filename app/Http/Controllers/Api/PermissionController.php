d754ccef11b56bdc7344fa8732f1f44e63d98825
JesterPC <ahmetkanbur@gmail.com>
2019-08-03 12:46:26 +0300

    add phpdoc blocks

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -13,16 +13,34 @@ use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
+    /**
+     * Display the specified resource.
+     *
+     * @param Permission $model
+     *
+     * @return PermissionResource
+     */
+    public function show(Permission $model)
+    {
+        return new PermissionResource($model);
+    }
 
+    /**
+     * Store a newly created model in storage.
+     *
+     * @param PermissionFormStoreRequest $request
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function store(PermissionFormStoreRequest $request)
     {
         $type = $request->get('type');
         $name = $request->get('name');
         $guard = 'api';
-        if( $type == 2 ){
-            $appModule = AppModule::findOrFail($request->get("app_module_id"));
-            $name = $appModule->permission_prefix.'.'.$name;
-            $guard = 'app_module_user';
+        if( $type == 2 ){ // app_module permission @todo implement ENUMS
+            $appModule  = AppModule::findOrFail($request->get("app_module_id"));
+            $name       = $appModule->permission_prefix.'.'.$name;
+            $guard      = 'app_module_user';
         }
         Permission::create([
             'name'              => $name,
@@ -33,25 +51,37 @@ class PermissionController extends Controller
         return new SuccessJSONResponseResource(null);
     }
 
-    public function show( Permission $model)
-    {
-        return new PermissionResource($model);
-    }
-
+    /**
+     * Update the specified location in storage.
+     *
+     * @param PermissionFormUpdateRequest  $request
+     * @param Permission                   $model
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function update(PermissionFormUpdateRequest $request, Permission $model ){
 
         $model->update($request->all());
         return new SuccessJSONResponseResource(null);
     }
 
-    public function destroy( Permission $model )
+    /**
+     * Remove permissions
+     *
+     * @param Permission $model
+     *
+     * @return SuccessJSONResponseResource
+     *
+     * @throws \Exception|\Throwable
+     */
+    public function destroy(Permission $model)
     {
         $users = User::permission($model)->get();
+        /** @var User $user */
         foreach( $users as $user ){
             $user->revokePermissionTo($model);
         }
         $model->delete();
         return new SuccessJSONResponseResource(null);
     }
-
 }
