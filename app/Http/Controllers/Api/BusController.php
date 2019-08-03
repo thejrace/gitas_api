d12a481baf94964235a5501959f30eda95274169
JesterPC <ahmetkanbur@gmail.com>
2019-08-03 12:43:09 +0300

    add phpdoc blocks

--- app/Http/Controllers/Api/BusController.php
+++ app/Http/Controllers/Api/BusController.php
@@ -9,27 +9,57 @@ use App\Http\Requests\BusFormStoreRequest;
 use App\Http\Requests\BusFormUpdateRequest;
 use App\Http\Resources\SuccessJSONResponseResource;
 
-
 class BusController extends Controller
 {
+    /**
+     * Display the specified resource.
+     *
+     * @param Bus $model
+     *
+     * @return BusResource
+     */
+    public function show(Bus $model)
+    {
+        return new BusResource($model);
+    }
+
+    /**
+     * Store a newly created model in storage.
+     *
+     * @param BusFormStoreRequest $request
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function store(BusFormStoreRequest $request)
     {
         Bus::create( $request->all() );
         return new SuccessJSONResponseResource(null);
     }
 
-    public function show($model)
-    {
-        return new BusResource($model);
-    }
-
+    /**
+     * Update the specified location in storage.
+     *
+     * @param BusFormUpdateRequest  $request
+     * @param Bus                   $model
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function update(BusFormUpdateRequest $request, Bus $model)
     {
         $model->update($request->all());
         return new SuccessJSONResponseResource(null);
     }
 
-    public function destroy($model)
+    /**
+     * Remove app module
+     *
+     * @param Bus $model
+     *
+     * @return SuccessJSONResponseResource
+     *
+     * @throws \Exception|\Throwable
+     */
+    public function destroy(Bus $model)
     {
         $model->delete();
         return new SuccessJSONResponseResource(null);
