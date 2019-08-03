b90331d40e374d8b4ee83b662424cdebd7ac18e6
JesterPC <ahmetkanbur@gmail.com>
2019-08-03 12:32:56 +0300

    add phpdocs and implement destroy method

--- app/Http/Controllers/Api/AppModuleController.php
+++ app/Http/Controllers/Api/AppModuleController.php
@@ -3,22 +3,48 @@
 namespace App\Http\Controllers\Api;
 
 use App\AppModule;
+use App\AppModuleUser;
 use App\Http\Requests\AppModuleFormStoreRequest;
 use App\Http\Requests\AppModuleFormUpdateRequest;
 use App\Http\Resources\AppModuleResource;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SuccessJSONResponseResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
 
 class AppModuleController extends Controller
 {
+    /**
+     * Get all AppModules
+     *
+     * @return AnonymousResourceCollection
+     */
     public function index()
     {
         return AppModuleResource::collection(AppModule::all());
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param AppModule $model
+     *
+     * @return AppModuleResource
+     */
+    public function show(AppModule $model)
+    {
+        return new AppModuleResource($model);
+    }
+
+    /**
+     * Store a newly created model in storage.
+     *
+     * @param AppModuleFormStoreRequest $request
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function store(AppModuleFormStoreRequest $request)
     {
         $attributes = $request->all();
@@ -27,29 +53,60 @@ class AppModuleController extends Controller
         return new SuccessJSONResponseResource(null);
     }
 
-    public function show($model)
-    {
-        return new AppModuleResource($model);
-    }
-
+    /**
+     * Update the specified location in storage.
+     *
+     * @param AppModuleFormUpdateRequest  $request
+     * @param AppModule                   $model
+     *
+     * @return SuccessJSONResponseResource
+     */
     public function update(AppModuleFormUpdateRequest $request, AppModule $model )
     {
         $model->update($request->all());
         return new SuccessJSONResponseResource(null);
     }
 
-    // @todo!!
-    public function destroy($id)
+    /**
+     * Remove app module
+     *
+     * @param AppModule $appModule
+     *
+     * @return SuccessJSONResponseResource
+     *
+     * @throws \Exception|\Throwable
+     */
+    public function destroy( AppModule $appModule )
     {
-        DB::transaction(function() use ($id) {
-            $model = AppModule::findOrFail($id);
-            $model->delete();
+        DB::transaction(function() use ($appModule) {
+            /** @var Permission $permission */
+            // get all permissions of app module
+            $permissionQuery = Permission::query();
+            $appModulePermissions = $permissionQuery
+                ->where('guard_name', 'app_module_user' )
+                ->where('name', 'LIKE', '%'.$appModule->permission_prefix.'.%' )->get();
 
-            // remove permissions and userpermissions
+            /** @var AppModule $appModule */
+            /** @var AppModuleUser $appModuleUser */
+            // for all module users, revoke their app module permissions
+            foreach( $appModule->users as $appModuleUser ){
+                foreach( $appModulePermissions as $permission ){
+                    if( $appModuleUser->hasPermissionTo($permission) ){
+                        $appModuleUser->revokePermissionTo($permission);
+                    }
+                }
+                // now delete app user
+                $appModuleUser->delete();
+            }
 
+            // remove all module's permissions
+            foreach( $appModulePermissions as $permission ){
+                $permission->delete();
+            }
 
-            // remove API user
+            // remove app module
+            $appModule->delete();
         });
-
+        return new SuccessJSONResponseResource(null);
     }
 }
