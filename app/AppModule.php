75b6cafae7f812e539682c6259c1395a0c1b4297
JesterPC <ahmetkanbur@gmail.com>
2019-08-03 12:32:40 +0300

    add phpdocs for model properties

--- app/AppModule.php
+++ app/AppModule.php
@@ -6,6 +6,34 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\AppModule
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $api_token
+ * @property string|null $permission_prefix
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\AppModuleUser[] $users
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule wherePermissionPrefix($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\AppModule whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class AppModule extends Authenticatable
 {
 
--- app/User.php
+++ app/User.php
@@ -6,6 +6,51 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string $api_token
+ * @property string|null $address
+ * @property string|null $city
+ * @property string|null $country
+ * @property string|null $phone_1
+ * @property string|null $phone_2
+ * @property string|null $citizenship_number
+ * @property string|null $date_of_birth
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereApiToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCitizenshipNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereDateOfBirth($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePhone1($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePhone2($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable, HasRoles;
