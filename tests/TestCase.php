628b38f65a5e6a3dd47dcd87cbfa2fe7baf3c08f
ipera <ahmetkanbur@gmail.com>
2019-08-20 11:41:36 +0300

    update helper test classes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Get a user for the tests
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|User
+     */
+    protected function getUser()
+    {
+        return User::first();
+    }
 }
