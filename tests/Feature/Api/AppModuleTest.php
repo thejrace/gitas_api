4b6aaa74c0106954b04ced672a12742893829990
ipera <ahmetkanbur@gmail.com>
2019-08-20 13:47:09 +0300

    test: add tests for AppModule and AppModuleUser endpoints

--- tests/Feature/Api/AppModuleTest.php
+++ tests/Feature/Api/AppModuleTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Api;
 
 use App\AppModule;
+use App\AppModuleUser;
 use App\Http\Resources\AppModuleResource;
+use App\Http\Resources\SuccessJSONResponseResource;
 use App\User;
 use Tests\ApiTestCase;
 
@@ -61,5 +63,66 @@ class AppModuleTest extends ApiTestCase
         $this->getJson($this->url() . $model->id)
             ->assertSuccessful()
             ->assertJsonFragment((new AppModuleResource($model))->jsonSerialize());
+    }
+
+    /** @test */
+    public function store_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var AppModule $model */
+        $model = factory(AppModule::class)->make();
+
+        $this->postJson($this->url(), $model->toArray())
+            ->assertSuccessful()
+            ->assertJsonFragment((new SuccessJSONResponseResource(null))->jsonSerialize());
+    }
+
+    /** @test */
+    public function update_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var AppModule $model */
+        $model = factory(AppModule::class)->create();
+
+        /** @var AppModule $modelData */
+        $modelData = factory(AppModule::class)->make();
+
+        $this->putJson($this->url() . $model->id, $modelData->toArray())
+            ->assertSuccessful()
+            ->assertJsonFragment((new SuccessJSONResponseResource(null))->jsonSerialize());
+
+        $this->assertDatabaseHas('app_modules', [
+            'id' => $model->id,
+            'name' => $modelData->name,
+            'description' => $modelData->description,
+            'permission_prefix' => $modelData->permission_prefix,
+        ]);
+    }
+
+    /** @test */
+    public function delete_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var AppModule $model */
+        $model = factory(AppModule::class)->create();
+
+        $moduleUser = factory(AppModuleUser::class)->create([
+            'app_module_id' => $model->id,
+        ]);
+
+        $this->delete($this->url() . $model->id )
+            ->assertSuccessful()
+            ->assertJsonFragment((new SuccessJSONResponseResource(null))->jsonSerialize());
+
+        $this->assertDatabaseMissing('app_modules', [
+            'id' => $model->id,
+        ]);
+
+        $this->assertDatabaseMissing('app_module_users', [
+            'id' => $moduleUser->id,
+        ]);
     }
 }
