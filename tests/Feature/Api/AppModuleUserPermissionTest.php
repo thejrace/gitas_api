8d7042abbfb65d92fee938e2cd847ab04687a375
ipera <ahmetkanbur@gmail.com>
2019-08-20 14:22:45 +0300

    test: add tests for app_module_user_permissions endpoint

--- tests/Feature/Api/AppModuleUserPermissionTest.php
+++ tests/Feature/Api/AppModuleUserPermissionTest.php
@@ -2,21 +2,133 @@
 
 namespace Tests\Feature\Api;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\AppModuleUser;
+use App\Http\Resources\PermissionResource;
+use App\Http\Resources\SuccessJSONResponseResource;
+use App\User;
+use Spatie\Permission\Models\Permission;
+use Tests\ApiTestCase;
 
-class AppModuleUserPermissionTest extends TestCase
+class AppModuleUserPermissionTest extends ApiTestCase
 {
     /**
-     * A basic feature test example.
+     * Get base Url for the API endpoint
      *
-     * @return void
+     * @return string
      */
-    public function testExample()
+    protected function url(): string
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        return '/api/app_module_user_permissions/';
     }
+
+    /** @test */
+    public function it_requires_authentication()
+    {
+        $this->getJson($this->url() . '1/1')
+            ->assertStatus(401)
+            ->assertExactJson([
+                'message' => 'Unauthenticated.',
+            ]);
+    }
+
+    /** @test */
+    public function it_requires_permission()
+    {
+        $this->actingAs(factory(User::class)->create(), 'api')
+            ->getJson($this->url() . '1')->dump()
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function give_permission_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var Permission $permission */
+        $permission = factory(Permission::class)->create([
+            'type' => 1, // app module user type
+            'guard_name' => 'app_module_user',
+        ]);
+
+        /** @var AppModuleUser $moduleUser */
+        $moduleUser = factory(AppModuleUser::class)->create();
+
+        $this->postJson($this->url() . $moduleUser->id . '/' . $permission->id )
+            ->assertSuccessful()
+            ->assertJsonFragment((new SuccessJSONResponseResource(null))->jsonSerialize());
+
+        $this->assertDatabaseHas('model_has_permissions', [
+            'permission_id' => $permission->id,
+            'model_id' => $moduleUser->id,
+        ]);
+    }
+
+    /** @test */
+    public function revoke_permission_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var Permission $permission */
+        $permission = factory(Permission::class)->create([
+            'type' => 1, // app module user type
+            'guard_name' => 'app_module_user',
+        ]);
+
+        /** @var AppModuleUser $moduleUser */
+        $moduleUser = factory(AppModuleUser::class)->create();
+
+        $this->deleteJson($this->url() . $moduleUser->id . '/' . $permission->id )
+            ->assertSuccessful()
+            ->assertJsonFragment((new SuccessJSONResponseResource(null))->jsonSerialize());
+
+        $this->assertDatabaseMissing('model_has_permissions', [
+            'permission_id' => $permission->id,
+            'model_id' => $moduleUser->id,
+        ]);
+    }
+
+    /** @test */
+    public function has_permission_test()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var Permission $permission */
+        $permission = factory(Permission::class)->create([
+            'type' => 1, // app module user type
+            'guard_name' => 'app_module_user',
+        ]);
+
+        /** @var AppModuleUser $moduleUser */
+        $moduleUser = factory(AppModuleUser::class)->create();
+
+        $moduleUser->givePermissionTo($permission);
+
+        $this->getJson($this->url() . $moduleUser->id . '/' . $permission->id )
+            ->assertSuccessful()
+            ->assertJsonFragment([
+                'hasPermission' => true,
+            ]);
+    }
+
+    /** @test */
+    public function get_permissions()
+    {
+        $this->actingAs($this->getUser(), 'api');
+
+        /** @var Permission $permission */
+        $permission = factory(Permission::class)->create([
+            'type' => 1, // app module user type
+            'guard_name' => 'app_module_user',
+        ]);
+
+        /** @var AppModuleUser $moduleUser */
+        $moduleUser = factory(AppModuleUser::class)->create();
+
+        $moduleUser->givePermissionTo($permission);
+
+        $this->getJson($this->url() . $moduleUser->id)
+            ->assertSuccessful()
+            ->assertJsonFragment((PermissionResource::collection($moduleUser->permissions))->jsonSerialize());
+    }
+
 }
