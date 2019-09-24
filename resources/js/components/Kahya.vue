ea37f6e86285da0d0fdbd239bdcdafc9d4ab38de
JesterPC <ahmetkanbur@gmail.com>
2019-09-24 19:44:32 +0300

    update

--- resources/js/components/Kahya.vue
+++ resources/js/components/Kahya.vue
@@ -32,7 +32,7 @@
         },
         mounted() {
             this.fetch();
-            this.timer = setInterval( this.fetch, 30000 );
+            this.timer = setInterval( this.fetch, 10000 );
         },
 
         data: () => ({
